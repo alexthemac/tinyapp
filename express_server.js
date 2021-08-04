ccf9f19e8eabacf8d97095ade6a5b2e976f6b137
Alex MacLure <almaclure@gmail.com>
2021-08-04 00:36:36 +0000

    Added logout functionality for user

--- express_server.js
+++ express_server.js
@@ -111,8 +111,12 @@ app.post("/login", (req, res) => {
   res.cookie('username', usernameEntered);
   //console.log();
   res.redirect(`/urls`);
-  
-})
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect(`/urls`);
+});
 
 
 
