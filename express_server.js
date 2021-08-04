55e81ab3f2980f381a72f3365a7f66cd8faa751e
Alex MacLure <almaclure@gmail.com>
2021-08-04 21:56:48 +0000

    Updated some HTML formatting to match compass example, still a work in progress

--- express_server.js
+++ express_server.js
@@ -23,7 +23,6 @@ const users = {
   }
 };
 
-
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 var cookieParser = require('cookie-parser'); //If not included, cookies is undefined initially and website crashes
@@ -39,6 +38,36 @@ function generateRandomString() {
   }
   return randomString;  
 }
+
+//Check if email is already in user "database"
+function emailAlreadyRegistered (email) {
+  for (user in users) {
+    if (email === users[user]['email']) {
+      return true;
+    }
+  }
+  return false;
+};
+
+//Check if password matches password in user "database"
+function passwordMatchRegistered (password) {
+  for (user in users) {
+    if (password === users[user]['password']) {
+      return true;
+    }
+  }
+  return false;
+};
+
+//Return user_id from user "database" via email lookup
+const userIDLookup = function (email) {
+  for (user in users) {
+    if (email === users[user]['email']) {
+      return user;
+    }
+  }
+};
+
 
 //Landing page displays Hello!
 app.get("/", (req, res) => {
@@ -64,7 +93,6 @@ app.get("/fetch", (req, res) => {
 //Displays all URLS in URL database
 app.get("/urls", (req, res) => {
   const templateVars = { 
-    //username: req.cookies["username"],
     user: users[req.cookies["user_id"]],
     urls: urlDatabase
     };
@@ -73,7 +101,6 @@ app.get("/urls", (req, res) => {
 //Displays create new URL page
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    //username: req.cookies["username"]
     user: users[req.cookies["user_id"]]
   };
   res.render("urls_new", templateVars);
@@ -82,19 +109,16 @@ app.get("/urls/new", (req, res) => {
 //Display single URL details (long and short)
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { 
-    //username: req.cookies["username"],
     user: users[req.cookies["user_id"]],
     shortURL: req.params.shortURL, 
     longURL: urlDatabase[req.params.shortURL]
   };
   res.render("urls_show", templateVars);
-
 });
 
 //Display registration URL page
 app.get("/register", (req, res) => {
   const templateVars = { 
-    //username: req.cookies["username"],
     user: users[req.cookies["user_id"]],
     shortURL: req.params.shortURL, 
     longURL: urlDatabase[req.params.shortURL]
@@ -111,7 +135,6 @@ app.get("/u/:shortURL", (req, res) => {
 //Displays login page
 app.get("/login", (req, res) => {
   const templateVars = { 
-    //username: req.cookies["username"],
     user: users[req.cookies["user_id"]],
     shortURL: req.params.shortURL, 
     longURL: urlDatabase[req.params.shortURL]
@@ -121,7 +144,6 @@ app.get("/login", (req, res) => {
 
 //Takes the data input into new /url/new and does something with it...
 app.post("/urls", (req, res) => {
-  //console.log(req.body); // Log the POST request body to the console {longURL: 'whatever entered in form'}
   const shortURLNew = generateRandomString(); //Creates a randomstring for the shortURLNew
   const longURLNew = req.body.longURL; //Takes the data entered in the form and stores it in longURLNew
   urlDatabase[shortURLNew] = longURLNew; //Creates new entry in urlDatabse object 
@@ -132,7 +154,6 @@ app.post("/urls", (req, res) => {
 //Deletes a url from the url database. (url to delete is :shortURL variable)
 app.post("/urls/:shortURL/delete", (req, res) => { 
   const urlToDelete = req.params["shortURL"];
-  console.log("deleted:",req.params["shortURL"]);
   delete urlDatabase[urlToDelete];
   res.redirect('/urls');
 });
@@ -141,19 +162,36 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 app.post("/urls/:shortURL", (req, res) => { 
   const shortURLToUpdate = req.params['shortURL']; //grabs the shortURL from the path
   const updatedLongURL = req.body['longURL']; //grabs the longURL that is entered in the form
-  // console.log("shortURL to be edited(req.params['shortURL']):", req.params['shortURL']);
-  // console.log("new longURL (req.body['longURL']):", req.body['longURL']);
   urlDatabase[shortURLToUpdate] = updatedLongURL; //updates urlDatabase with this data
   res.redirect(`/urls`); //redirects to /urls page once I edit one
 });
 
-//
+//Logs in user
 app.post("/login", (req, res) => {
-  const usernameEntered = req.body["username"];
-  res.cookie('username', usernameEntered);
-  res.redirect(`/urls`);
+  const email = req.body['email'];
+  const password = req.body['password'];
+
+  //If email has not been registerd, throw error
+  if (!emailAlreadyRegistered(email)) {
+    //Sets status code to 403 
+    res.status(403);
+    //Displays 'Response: 400 Bad Request on the /login page
+    res.send('Response: Email not registered');
+  //If email has been registered, but wrong password entered, throw error
+  } else if (emailAlreadyRegistered(email) && !passwordMatchRegistered(password)) {
+    //Sets status code to 403 
+    res.status(403);
+    //Displays 'Response: 400 Bad Request on the /login page
+    res.send('Response: Password does not match records');
+  //If the email has been registered and password is correct, update cookie with user_id to id of email entered
+  } else {
+    const id = userIDLookup(email);
+    res.cookie('user_id', id);
+    res.redirect(`/urls`);
+  }
 });
 
+//Logsout user (clears cookie with id info of user)
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
   res.redirect(`/urls`);
@@ -163,13 +201,7 @@ app.post("/logout", (req, res) => {
 app.post("/register", (req, res) => {
   const {email, password} = req.body; //Grabs the email and password entered in /register
   const id = generateRandomString(); //Creates a unique id for the new user
-  let emailInUsers = false;
-  //Check if email is already in user "database"
-  for (user in users) {
-    if (email === users[user]['email']) {
-      emailInUsers = true;
-    }
-  };
+
   //if email is blank or password is blank, display error
   if (email === "" || password === "") {
     //Sets status code to 400 (bad request)
@@ -177,7 +209,7 @@ app.post("/register", (req, res) => {
     //Displays 'Response: 400 Bad Request on the /register page
     res.send('Response: Not a valid email');
   //if email is already in user "database", display error
-  } else if (emailInUsers === true) {
+  } else if (emailAlreadyRegistered(email) === true) {
     //Sets status code to 400 (bad request)
     res.status(400);
     //Displays 'Response: 400 Bad Request on the /register page
@@ -190,7 +222,6 @@ app.post("/register", (req, res) => {
     password
     };
     //Sets a user_id cookie to the newly created id
-    console.log(users);
     res.cookie('user_id', id);
     res.redirect(`/urls`);
   }
