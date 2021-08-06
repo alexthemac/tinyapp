630204d0a60847f28c79df7227ce2c124323b029
Alex MacLure <almaclure@gmail.com>
2021-08-06 01:37:12 +0000

    Deleted unnecessary lines in helpers.js

--- helpers.js
+++ helpers.js
@@ -1,15 +1,13 @@
-
-
 //Check if email is already in user "database"
 const getUserByEmail = function(email, database) {
-  for (user in database) {
+  for (const user in database) {
     if (email === database[user]['email']) {
       return user;
     }
   }
 };
 
+module.exports = { getUserByEmail };
+
 //For future?
 // module.exports = { generateRandomString, getUserByEmail, hashedPasswordLookup, userIDLookup, filterURLDatabase }
-
-module.exports = { getUserByEmail };
